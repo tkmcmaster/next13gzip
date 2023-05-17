78a40d5bfb9e905171e0a07b689c51a4e847d58b
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-05-17 14:55:06 -0600

    Added repro cases

--- pages/api/gzip.ts
+++ pages/api/gzip.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream';
+import { createGzip } from 'zlib';
 
 type Data = {
   name: string
@@ -12,7 +13,8 @@ export default function handler(
 ) {
   res.status(200);
   const json: Data = { name: 'John Doe' };
-  const content = Readable.from(JSON.stringify(json));
+  const content = Readable.from(JSON.stringify(json)).pipe(createGzip());
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Encoding", "gzip");
   res.send(content);
 }
